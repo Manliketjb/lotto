84afebd895723a926cf50bbda82c3d7c0141f4b7
ManLikeTJB <82594996+Manliketjb@users.noreply.github.com>
2021-10-07 23:37:49 +0100

    Update server.lua

--- lotto/server.lua
+++ lotto/server.lua
@@ -14,7 +14,6 @@ RegisterServerEvent('lotto:win')
 AddEventHandler('lotto:win', function()
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
-	local array = {750, 640, 510, 0, 50, 300, 600, 1, 2, 10, 25, 90, 60, 65, 3, 290, 345, 691, 81, 329, 70, 20, 4, 5, 465, 470}
-	local money = array[math.random(26)]
-	Player.Functions.AddMoney('cash',Config.Payment)
+	local money = math.random(330, 2000)
+	Player.Functions.AddMoney('cash', Config.Payment)
 end)
